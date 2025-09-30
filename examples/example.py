9b0448faf9ae5cbfd8c6ab07b4135aa6cb284e29
Daulet Amirkhanov <damirkhanov01@gmail.com>
2025-09-30 10:32:40 +0100

    refactor: rename package to langgraph-integration-cognee and update dependencies

--- examples/example.py
+++ examples/example.py
@@ -1,7 +1,7 @@
 # Import core components
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages import HumanMessage
-from langgraph_cognee import get_sessionized_cognee_tools
+from langgraph_integration_cognee import get_sessionized_cognee_tools
 import asyncio
 from dotenv import load_dotenv
 import os
